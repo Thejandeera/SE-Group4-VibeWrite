df5882380492d92303bc9deb2e1abe9b2c69bd46
Thejandeera <thejan1010@gmail.com>
2025-08-21 22:53:07 +0530

    initial

---
+++ backend/src/main/java/com/group4/vibeWrite/VibeWriteApplication.java
@@ -0,0 +1,13 @@
+package com.group4.vibeWrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeWriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeWriteApplication.class, args);
+	}
+
+}
