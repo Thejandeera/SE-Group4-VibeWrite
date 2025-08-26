1d9716da698ead74c39e2ff4ffd16aced8a2aad8
Thejandeera <thejan1010@gmail.com>
2025-08-26 20:50:49 +0530

    updated App jsx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   server: {
     port: 5173,
-    strictPort: true // will fail if 5173 is in use instead of picking another port
+    strictPort: true 
   }
 })
